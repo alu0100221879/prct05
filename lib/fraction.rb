ae967205ce3129265baafd9c034dd2e637554244
Airam Lozano Alonso <powerthatbe@gmail.com>
2015-10-30 03:16:53 +0000

    Constructor y conversión a cadena

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -1,3 +1,22 @@
+# encoding: utf-8
+
 class Fraction
+
+	attr_reader :p, :q
+	
+	def initialize(p, q)
+        
+		raise ArgumentError, "El numerador debe ser un número entero" unless p.is_a? Integer
+		raise ArgumentError, "El denominador debe ser un número entero" unless q.is_a? Integer
+		
+		raise ArgumentError, "El denominador debe ser un número entero positivo" unless q > 0
+		
+		@p, @q = p, q
+		
+	end
+	
+	def to_s
+		"#{p}/#{q}"
+	end
 
 end
--- test/tc_fraction.rb
+++ test/tc_fraction.rb
@@ -1,6 +1,31 @@
+# encoding: utf-8
+
 require 'lib/fraction.rb'
 require 'test/unit'
 
-class TestFraction < Test::Unit::Testcase
+class TestFraction < Test::Unit::TestCase
+
+	def setup
+		@medio = Fraction.new(1, 2)
+		@cuarto = Fraction.new(1, 4)
+	end
+	
+	def tear_down
+		# Nada
+	end
+	
+	def test_simple
+		assert_equal("1/2", @medio.to_s)
+	end
+	
+	def test_numerator_check
+		assert_raise(ArgumentError) {Fraction.new('1', 2)}
+	end
+	
+	def test_denominator_check
+		assert_raise(ArgumentError) {Fraction.new(1, '2')}
+		assert_raise(ArgumentError) {Fraction.new(1, 0)}
+		assert_raise(ArgumentError) {Fraction.new(1, -5)}
+	end	
 
 end
