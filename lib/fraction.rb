c3225a7ac417ea52b396f1c18cf08740c94dbbac
Airam Lozano Alonso <powerthatbe@gmail.com>
2015-10-30 05:47:04 +0000

    Añadidos comentarios

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -2,6 +2,7 @@
 
 class Fraction
 
+	# Atributos de sólo lectura
 	attr_reader :p, :q
 	
 	def initialize(p, q)
@@ -11,13 +12,14 @@ class Fraction
 		
 		raise ArgumentError, "El denominador debe ser un número entero positivo" unless q > 0
 		
+		# Simplifica el numerador y el denominador antes de almacernarlos
 		r = Fraction.gcd(p.abs, q)
 		@p, @q = p / r, q / r
 		
 	end
 	
 	def to_s
-		return @q.abs == 1 ? "#{p * q}" : "#{p}/#{q}"
+		return @q.abs == 1 ? "#{p * q}" : "#{p}/#{q}" # Simplifica a número entero cuando el denominador es la unidad
 	end
 	
 	def self.gcd(n1, n2)
@@ -26,6 +28,7 @@ class Fraction
 		
 		p, q = n1, n2
 		
+		# Algoritmo de Euclides: gcd(a, b) == gcd(b, a % b) y gcd(a, 0) = a
 		while q != 0
 			r = p % q
 			p = q
@@ -38,7 +41,7 @@ class Fraction
 	
 	def self.lcm(n1, n2)		
 		raise ArgumentError, "Los números deben ser enteros" unless (n1.is_a? Integer) && (n2.is_a? Integer)
-		return (n1 * n2) / Fraction.gcd(n1, n2)
+		return (n1 * n2) / Fraction.gcd(n1, n2) # n1 * n2 = gcd(n1, n2) * lcm(n1, n2)
 	end
 	
 	def *(n)
@@ -49,7 +52,7 @@ class Fraction
 	def /(n)
 		raise ArgumentError, "Una fracción sólo se puede dividir por otra fracción" unless n.instance_of? Fraction
 		raise ArgumentError, "No se puede dividir entre 0" if n.p == 0
-		return Fraction.new(@p * n.q, @q * n.p)
+		return Fraction.new(@p * n.q, @q * n.p) # Multiplicación cruzada
 	end
 	
 	def -@
@@ -60,6 +63,7 @@ class Fraction
 		
 		raise ArgumentError, "Una fracción sólo se puede sumar a otra fracción" unless n.instance_of? Fraction
 		
+		# Calcula los numeradores tras reducir las fracciones a común denominador
 		denominator = Fraction.lcm(@q, n.q)
 		numerator_s = @p * (denominator / @q)
 		numerator_n = n.p * (denominator / n.q)
@@ -70,7 +74,7 @@ class Fraction
 	
 	def -(n)
 		raise ArgumentError, "Una fracción sólo se puede restar por otra fracción" unless n.instance_of? Fraction
-		return self + (-n)
+		return self + (-n) # a - b = a + (-b)
 	end
 
 end
